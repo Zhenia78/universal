101dc78edc9cd5384ddf697cdd1f0a0ddcd2fcb2
Zhenia78 <zbaklanov2@gmail.com>
2020-08-11 22:30:01 +0300

    create: header, todays-news, tabs

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+  $(".burger").on("click", function () {
+    $(".burger__button").toggleClass("menu-active");
+    $(".dropdown-menu").toggleClass("menu-active");
+    $("body").toggleClass("menu-active");
+  });
+
+  $(".tabs-item").on("click", function () {
+    $(".tabs-item").removeClass("tab-active");
+    $(".tabs-block").removeClass("tab-active");
+    $(this).addClass("tab-active");
+    $("#" + $(this).attr("data-target")).addClass("tab-active");
+  });
+
+
+  var swiper = new Swiper('.hot-news__swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.hot-news__button-next',
+      prevEl: '.hot-news__button-prev',
+    },
+  });
+
+  var swiper = new Swiper('.article-slider__container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.article-slider__button-next',
+      prevEl: '.article-slider__button-prev',
+    },
+  });
+
+});
